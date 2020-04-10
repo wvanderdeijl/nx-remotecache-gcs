b315ba3d2c0425624b49cd6e882b41b899df9ab2
Wilfred <wilfred@vanderdeijl.com>
2020-04-10 06:33:35 +0200

    initial version
    clean up
    
    
    fix mkdirp call
    
    fix mkdirp call
    
    
    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-nx-gcs-cache
+# nx-remotecache-gcs
+
+A task runner for [@nrwl/nx](https://nx.dev/react) that uses a Google Cloud Storage bucket as a remote cache, so all team members and CI servers can share a single cache. The concept and benefits of [computation caching](https://nx.dev/angular/guides/computation-caching) are explained in the NX documentation.
+
+## setup
+
+```
+npm install --save-dev nx-remotecache-gcs
+```
+
+create a Google Cloud Storage bucket. Since this is only a cache, there is no need for a dual- or multi-region bucket, so choose a  single [location](https://cloud.google.com/storage/docs/locations) near you.
+
+```
+gsutil mb -p [PROJECT_ID] -l [BUCKET_LOCATION] -b on gs://[BUCKET_NAME]/
+```
+
+setup a [lifecycle rule](https://cloud.google.com/storage/docs/managing-lifecycles) for your storage bucket to automatically delete old files. If you want to use our suggested auto-delete-after-30-days rule, you can simply use the json that is included:
+
+```
+gsutil lifecycle set node_modules/nx-remotecache-gcs/lifecycle.json gs://[BUCKET_NAME]
+```
+
+by default all viewers, editors and owners of your Google Cloud project can read and/or write to the bucket. You could also [restrict who can read and write](https://cloud.google.com/storage/docs/access-control/using-iam-permissions) the bucket to only allow certain users or (build server) service accounts.
+
+finally, add `tasksRunnerOptions` in your `nx.json` file
+
+```json
+{
+    "projects": {
+        ...
+    },
+    "tasksRunnerOptions": {
+        "default": {
+            "runner": "nx-remotecache-gcs",
+            "options": {
+                "bucket": "gs://NAME-OF-YOUR-STORAGE-BUCKET",
+                "cacheableOperations": [
+                    "build",
+                    "test",
+                    "lint",
+                    "e2e"
+                ]
+            }
+        }
+    }
+}
+
+```
+
+run a build and see if files end up in your cache storage bucket:
+
+```
+nx run-many --target=build --all
+```
---
+++ index.ts
@@ -0,0 +1,68 @@
+import defaultTaskRunner from '@nrwl/workspace/tasks-runners/default';
+import { Storage } from '@google-cloud/storage';
+import { join, dirname, relative } from 'path';
+import { promises } from 'fs';
+import mkdirp from 'mkdirp';
+
+export default function runner(
+    tasks: Parameters<typeof defaultTaskRunner>[0],
+    options: Parameters<typeof defaultTaskRunner>[1] & { bucket?: string },
+    context: Parameters<typeof defaultTaskRunner>[2],
+) {
+    if (!options.bucket) {
+        throw new Error('missing bucket property in runner options. Please update nx.json');
+    }
+    const bucket = new Storage().bucket(options.bucket);
+    return defaultTaskRunner(tasks, { ...options, remoteCache: { retrieve, store } }, context);
+
+    async function retrieve(hash: string, cacheDirectory: string): Promise<boolean> {
+        try {
+            const commitFile = bucket.file(`${hash}.commit`);
+            if (!(await commitFile.exists())[0]) {
+                return false;
+            }
+            const [files] = await bucket.getFiles({ prefix: `${hash}/` });
+            await Promise.all(files.map(download));
+            await download(commitFile); // commit file after we're sure all content is downloaded
+            console.log(`retrieved ${files.length + 1} files from cache gs://${bucket.name}/${hash}`);
+            return true;
+        } catch (e) {
+            console.log(e);
+            console.log(`WARNING: failed to download cache from ${bucket.name}: ${e.message}`);
+            return false;
+        }
+
+        async function download(file: import('@google-cloud/storage').File) {
+            const destination = join(cacheDirectory, file.name);
+            await mkdirp(dirname(destination));
+            await file.download({ destination });
+        }
+    }
+
+    async function store(hash: string, cacheDirectory: string): Promise<boolean> {
+        const tasks: Promise<any>[] = [];
+        try {
+            await uploadDirectory(join(cacheDirectory, hash));
+            await Promise.all(tasks);
+            await bucket.upload(join(cacheDirectory, `${hash}.commit`)); // commit file once we're sure all content is uploaded
+            console.log(`stored ${tasks.length + 1} files in cache gs://${bucket.name}/${hash}`);
+            return true;
+        } catch (e) {
+            console.log(`WARNING: failed to upload cache to ${bucket.name}: ${e.message}`);
+            return false;
+        }
+
+        async function uploadDirectory(dir: string) {
+            for (const entry of await promises.readdir(dir)) {
+                const full = join(dir, entry);
+                const stats = await promises.stat(full);
+                if (stats.isDirectory()) {
+                    await uploadDirectory(full);
+                } else if (stats.isFile()) {
+                    const destination = relative(cacheDirectory, full);
+                    tasks.push(bucket.upload(full, { destination }));
+                }
+            }
+        }
+    }
+}
